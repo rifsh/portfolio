4fa585603c0eeda077cbf783be82d061617bb1cf
rifsh <rifshmuhammed@gmail.com>
2024-08-09 17:37:42 +0530

    version4

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
@@ -12,7 +16,19 @@ export class ContactComponent {
   options: string[] = ['Frontend', 'Backend', 'Fullstack'];
   selectedOption: string | null = null;
   isDropdownOpen = false;
-  mail: string = 'rifshmuhammed@gmail.com'
+  mail: string = 'rifshmuhammed@gmail.com';
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      fName: ['', Validators.required],
+      lName: ['', Validators.required],
+      eMail: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
   toggleDropdown() {
     this.isDropdownOpen = !this.isDropdownOpen;
   }
@@ -20,5 +36,10 @@ export class ContactComponent {
   selectOption(option: string) {
     this.selectedOption = option;
     this.isDropdownOpen = false;
+  }
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+    
   }
 }
